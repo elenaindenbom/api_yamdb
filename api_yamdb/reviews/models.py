841ae53e0243dd0c72e9cfa2082e3814b86720a1
Åëåíà Èíäåíáîì <elenaindenbom@gmail.com>
2022-08-25 18:49:25 +0300

    Добавлены модели отзывов и комментариев

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+SCORES = [i for i in range(1, 11)]
+
+
+class Review(models.Model):
+    text = models.CharField('Текст отзыва', max_length=200)
+    score = models.IntegerField('Оценка', choices=SCORES)
+    title = models.ForeignKey(
+        Titles,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+
+    class Meta:
+        ordering = ["-pub_date"]
+
+    def __str__(self):
+        return self.text
+
+
+class Comment(models.Model):
+    text = models.CharField('Текст комментария', max_length=200)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+
+    class Meta:
+        ordering = ["-pub_date"]
+
+    def __str__(self):
+        return self.text

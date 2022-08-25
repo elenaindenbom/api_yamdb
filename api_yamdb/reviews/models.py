754d644b37d31bad3abc70f486ca199ad58475c0
Эльдар Мустафутдинов <emustafutdinov@gmail.com>
2022-08-25 15:30:54 +0300

    Создали user,выполнили миграции

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -3,25 +3,33 @@ from django.db import models
 
 
 class User(AbstractUser):
-
     USER = 'user'
     MODER = 'moderator'
     ADMIN = 'admin'
 
-    USER_ROLES = (
+    USER_ROLES = [
         (MODER, 'Модератор'),
         (ADMIN, 'Администратор'),
         (USER, 'Пользователь'),
-    )
+    ]
 
     bio = models.TextField(
         'Биография',
         blank=True,
-        null=True,
+        null=True
     )
+
     role = models.CharField(
-        'Пользовательские роли',
-        max_length= 50,
+        'Роль пользователя',
+        max_length=50,
         default=USER,
         choices=USER_ROLES
     )
+
+    email = models.EmailField(
+        'Почта',
+        unique=True
+    )
+
+    def __str__(self):
+        return self.username
